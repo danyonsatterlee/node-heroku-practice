53194540e2d490a8e11a912535275143c79f1283
Danyon Satterlee <>
2018-06-18 11:04:51 -0600

    created projects page and route

--- server.js
+++ server.js
@@ -50,6 +50,11 @@ app.get('/about', (req, res)=>{
     });
 });
 
+app.get('/projects', (req, res)=>{
+    res.render('projects.hbs', {
+        pageTitle: 'Project Page',
+    });
+});
 app.get('/bad', (req, res)=>{
     res.send({
         message: 'this is a bad request'
